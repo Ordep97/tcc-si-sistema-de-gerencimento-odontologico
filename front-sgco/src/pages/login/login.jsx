97fdc8bd4cb1bbdabfd0342ba3d31d36755f3b49
AndersonRodriguess <100146733+AndersonRodriguess@users.noreply.github.com>
2022-10-12 19:19:17 -0300

    WIP

--- front-sgco/src/pages/login/login.jsx
+++ front-sgco/src/pages/login/login.jsx
@@ -10,7 +10,6 @@ import "./login-style.css";
                 <h1>LOGIN</h1>
                 <label for="email">E-mail</label> 
                 <input type="email" name="email" id="email" placeholder="Digite seu e-mail" autocomplete="off"/>
-                {/* placeholder é uma "dica" para orientar o usuário o que digitar */}
                 <label for="password">Senha</label>
                 <input type="password" name="password" id="password" placeholder="Digite a sua senha"/>
                 <input type="submit" value="Entrar"/>            
