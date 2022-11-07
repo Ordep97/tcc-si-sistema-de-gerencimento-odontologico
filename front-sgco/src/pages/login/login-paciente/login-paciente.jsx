e37dd60a72df7541ff0311645224e26075c89a43
AndersonRodriguess <100146733+AndersonRodriguess@users.noreply.github.com>
2022-11-07 17:27:12 -0300

    WIP

--- front-sgco/src/pages/login/login-paciente/login-paciente.jsx
+++ front-sgco/src/pages/login/login-paciente/login-paciente.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./login-paciente-style.css";
+import dadospaciente from '../../../assets/login-paciente/dados-paciente.png';
+import consultaspaciente from '../../../assets/login-paciente/consultas-paciente.png'
 
 
   const LoginPacientePage = () => {
@@ -11,20 +13,25 @@ import "./login-paciente-style.css";
             <h1>Bem vindo, "Usuário"...</h1>
 
                 <div class="consultas">
+
                     <section class="meus-dados">
-                    <h2>Altere aqui seus dados já cadastrados</h2>
-                    <p>Aqui poderá visualizar e alterar os seus dados cadastrados.</p>
-                    <input type="submitMeusDados" value="Meus dados"/>
+                        <div class="dados-paciente-img">
+                            <img src={dadospaciente} alt="dados-paciente-img"></img>
+                        </div>
+                        <h2>Altere aqui seus dados já cadastrados</h2>
+                        <p>Aqui poderá visualizar e alterar os seus dados cadastrados.</p>
+                        <input type="submitMeusDados" value="Meus dados"/>
+                    </section>
 
-                </section>
+                    <section class="minhas-consultas">
+                        <div class="consultas-paciente-img">
+                            <img src={consultaspaciente} alt="consultas-paciente-img"></img>
+                        </div>
+                        <h2>Veja todas as consultas que tem marcadas</h2>
+                        <p>Aqui você poderá visualizar e/ou excluir consultas marcadas para você.</p>
+                        <input type="submitMinhasConsultas" value="Minhas consultas"/>
 
-                <section class="minhas-consultas">
-
-                    <h2>Veja todas as consultas que tem marcadas</h2>
-                    <p>Aqui você poderá visualizar e/ou excluir consultas marcadas para você.</p>
-                    <input type="submitMinhasConsultas" value="Minhas consultas"/>
-
-                </section>
+                    </section>
 
                 </div>
 
